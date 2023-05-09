bfea987c271e172291df89591cb58555d01bb421
Jamie <adams.j.6385@gmail.com>
2023-05-09 22:59:44 +0100

    Try class based points

--- points.js
+++ points.js
@@ -1,88 +1,99 @@
 class Point {
-    constructor(id, title, colour, toi) {
+    constructor({id, title, colour, toi, buttonlabel, position, gmaps}) {
         this.id = id;
         this.title = title;
         this.colour = colour;
+        this.buttonlabel = buttonlabel;
+        this.position = position;
+        this.toi = toi;
+        this.gmaps = gmaps;
     }
 }
 
 class TOI {
-    constructor(dateTaken) {
+    constructor({id, dateTaken, dateAdded, dateUpdated, direction, image, description}) {
+        this.id = id;
         this.dateTaken = dateTaken;
+        this.dateAdded = dateAdded;
+        this.dateUpdated = dateUpdated;
+        this.image = image;
+        this.direction = direction;
+        this.description = description;
     }
 };
 
-export const points = [
-    {
-        "id": "1",
-        "position": {
-            "lat": 50.701870298467696,
-            "lng": -1.2895803407729292
-        },
-        "title": "Newport Rail Tunnel",
-        "buttonlabel": "",
-        "color": "#ff0000",
-		"date-added": "",
-		"date-updated": "",
-        "g-maps": "",
-		"history": [
-			{
-                "id": "1-1",
-				"date-taken": "2022-01-01",
-				"image": "https://source.unsplash.com/1600x900/?beach",
-				"credit": {
-                    "name": "",
-                    "email" : "",
-                    "source" : "",
-                },
-				"description": "An image of course",
-				"date-added": "",
-				"date-updated": "",
-                "direction": ""
-			},
-            {
-                "id": "1-1",
-				"date-taken": "2022-01-01",
-				"image": "https://source.unsplash.com/1600x900/?sea",
-				"credit": {
-                    "name": "",
-                    "email" : "",
-                    "source" : "",
-                },
-				"description": "",
-				"date-added": "",
-				"date-updated": "",
-                "direction": ""
-			}
-		]
-    },
-    {
-        "id": "2",
-        "position": {
-            "lat": 50.59802389332379,
-            "lng": -1.2083137035369875
-        },
-        "title": "Ventnor Rail Tunnel",
-        "buttonlabel": "",
-        "color": "#ff0000",
-		"date-added": "",
-		"date-updated": "",
-        "g-maps": "",
-		"history": [
-			{
-                "id": "2-1",
-				"date-taken": "2022-01-01",
-				"image": "https://source.unsplash.com/1600x900/?beach",
-				"credit": {
-                    "name": "",
-                    "email" : "",
-                    "source" : "",
-                },
-				"description": "",
-				"date-added": "",
-				"date-updated": "",
-                "direction": ""
-			}
-		]
+class Credit {
+    constructor({name, email, source}) {
+        this.name = name;
+        this.email = email;
+        this.source = source;
     }
+}
+
+class Position {
+    constructor({lat, lng}) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
+
+const NewportRailTunnel = new Point({
+    id: 1,
+    title: "Newport Rail Tunnel",
+    colour: "#ff0000",
+    buttonlabel: "",
+    position: new Position({
+        lat: 50.701870298467696,
+        lng: -1.2895803407729292
+    }),
+    toi: [
+        new TOI({
+            id: 1,
+            dateTaken: "2022-01-01",
+            dateAdded: "",
+            dateUpdated: "",
+            direction: "",
+            image: "https://source.unsplash.com/1600x900/?sea",
+            credit: new Credit({
+                name: "",
+                email: "",
+                source: ""
+            }),
+            description: "",
+        })
+    ]
+})
+const VentnorRailTunnel = new Point({
+    id: 2,
+    position: new Position({
+        lat: 50.59802389332379,
+        lng: -1.2083137035369875
+    }),
+    title: "Ventnor Rail Tunnel",
+    buttonlabel: "",
+    colour: "#ff0000",
+    dateAdded: "",
+    dateUpdated: "",
+    gmaps: "",
+    toi: [
+        new TOI({
+            id: 1,
+            dateTaken: "2022-01-01",
+            image: "https://source.unsplash.com/1600x900/?beach",
+            credit: new Credit({
+                name: "",
+                email : "",
+                source : "",
+            }),
+            description: "",
+            dateAdded: "",
+            dateUpdated: "",
+            direction: ""
+        })
+    ]
+})
+
+export const points = [
+    NewportRailTunnel,
+    VentnorRailTunnel
 ];
