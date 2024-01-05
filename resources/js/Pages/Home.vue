e0bc31b55d1a3020d7d88b75278520ab1136bc8a
jlabs <adams.j.6385@gmail.com>
2024-01-05 14:44:28 +0000

    Test polygon

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -36,6 +36,12 @@ onMounted(() => {
         fillOpacity: 0.5,
         radius: 500
     }).addTo(map);
+    // Test polygon
+    var polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+    ]).addTo(map);
 })
 
 </script>
