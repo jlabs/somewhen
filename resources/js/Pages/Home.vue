f27e3c2899305b57c34604c533ed5b214ed2fcf3
jlabs <adams.j.6385@gmail.com>
2024-01-05 14:41:54 +0000

    Setup leaflet map

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
+import { onMounted } from 'vue';
 
 defineProps({
     canLogin: {
@@ -17,8 +18,24 @@ defineProps({
         required: true,
     },
 });
+
+onMounted(() => {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+})
+
 </script>
 
 <template>
     <Head title="Welcome" />
+    
+    <div id="map"></div>
 </template>
+
+<style>
+    #map { height: 180px; }
+</style>
