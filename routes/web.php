6b97441712e7c8a56d3688a058b7fe6ef4086ee9
jlabs <adams.j.6385@gmail.com>
2024-01-10 20:56:02 +0000

    Parse locations to homepage

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Models\Location;
 
 Route::get('/', function () {
     return Inertia::render('Home', [
@@ -22,6 +12,7 @@ Route::get('/', function () {
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
+        'locations' => Location::all()
     ]);
 });
 
