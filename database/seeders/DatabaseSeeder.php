d6f772ea83d15ff915b1c7adaa9756f9eb1d5ba9
jlabs <adams.j.6385@gmail.com>
2024-01-13 21:24:38 +0000

    Seed reasonably accurate data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,26 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Location::factory()->create([
+            'title' => 'Newport Rail Tunnel',
+            'colour' => '#ff0000',
+            'button_label' => 'Newport Rail Tunnel',
+            'coordinates' => [
+                'lat' => '50.701870298467696',
+                'lng' => '-1.2895803407729292',
+            ],
+            'gmaps_url' => fake()->url()
+        ]);
+        \App\Models\Location::factory()->create([
+            'title' => 'Ventnor Rail Tunnel',
+            'colour' => '#ff0000',
+            'button_label' => 'Newport Rail Tunnel',
+            'coordinates' => [
+                'lat' => '50.59802389332379',
+                'lng' => '-1.2083137035369875',
+            ],
+            'gmaps_url' => fake()->url()
+        ]);
     }
 }
