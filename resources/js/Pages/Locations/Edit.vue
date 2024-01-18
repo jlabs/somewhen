369cfb822dee95b0958e7177b3da0a8495255425
jlabs <adams.j.6385@gmail.com>
2024-01-18 23:48:53 +0000

    Define props of location to edit

--- resources/js/Pages/Locations/Edit.vue
+++ resources/js/Pages/Locations/Edit.vue
@@ -3,6 +3,13 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import { useForm } from '@inertiajs/vue3'
 
+defineProps({
+    location: {
+        type: Object,
+        required: true
+    }
+})
+
 const form = useForm({
     title: null,
     coordinates: null,
@@ -21,11 +28,11 @@ const form = useForm({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100 w-1/2 mx-auto">
-                        <form @submit.prevent="form.post('/locations')" class="flex flex-col">
-                            <input type="text" v-model="form.title" class="text-black" placeholder="Title">
+                        <form @submit.prevent="form.post('/locations/create')" class="flex flex-col">
+                            <input type="text" v-model="location.title" class="text-black" placeholder="Title">
                             <div v-if="form.errors.title">{{ form.errors.title }}</div>
                             
-                            <input type="text" v-model="form.coordinates" class="text-black" placeholder="Coordinates">
+                            <input type="text" v-model="location.coordinates" class="text-black" placeholder="Coordinates">
                             <div v-if="form.errors.coordinates">{{ form.errors.coordinates }}</div>
                             
                             <button type="submit" class="block bg-orange-500 p-2 rounded my-2" :disabled="form.processing">Login</button>
