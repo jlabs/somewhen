99a6cb6bf3c937132436286389faef94ad522600
jlabs <adams.j.6385@gmail.com>
2024-01-20 23:58:27 +0000

    Test leaflet page

--- resources/js/Pages/Leaflet.vue
+++ resources/js/Pages/Leaflet.vue
@@ -1,21 +1,12 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-import { onMounted, ref } from 'vue';
-//import { points } from '../points.js'
 
-const props = defineProps({
-    locations: {
-        type: Array,
-        required: true
-    }
-});
+import { onMounted, ref } from 'vue';   
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
-var map;
-
-const locationRef = ref({});
-
-onMounted(() => {
-    map = L.map('map')
+function setupLeafletMap() {
+    const mapDiv = L.map("mapContainer").setView(center, 13);
+    mapDiv = L.map('map')
         .setView([
             50.7065055999999998448402038775384426116943359375,
             -1.296561063744878339321076055057346820831298828125
@@ -25,106 +16,23 @@ onMounted(() => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map); 
+}
 
-    // Click events
-    map.on('click', onMapClick);
-
-    const bounds = [];
-
-    props.locations.forEach(function (marker) {
-        var circle = L.circle([marker.coordinates.lat, marker.coordinates.lng], {
-            color: marker.colour,
-            fillColor: marker.colour,
-            fillOpacity: 0.8,
-            radius: 300
-        }).addTo(map);
-        circle.bindPopup(`
-            <div class='text-xl'>${marker.title}</div>
-            <div>${marker.moments.length} moments</div>
-            <div onclick='Boo(${marker.id})'>View</div>
-        `);
-        // Add a click event listener to the circle
-        circle.on('click', function (e) {
-            // 'e' is the event object, and 'e.latlng' contains the coordinates where the click occurred
-            //alert('Circle clicked at: ' + e.latlng.toString());
-            GetLocation(marker.id);
-        });
-
-        bounds.push([marker.coordinates.lat, marker.coordinates.lng]);
-    });
-    map.fitBounds(bounds);
+onMounted(() => {
+    setupLeafletMap();
 })
-
-async function GetLocation(id) {
-    const response = await fetch(`/api/locations/${id}`);
-    const location = await response.json();
-}
-
-function onMapClick(e) {
-    var popup = L.popup();
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(map);
-}
-
-function Boo() {
-    console.log('boo');
-}
-
-async function panTo(id) {
-    const response = await fetch(`/api/locations/${id}`);
-    //const location = await response.json();
-
-    locationRef.value = await response.json();
-
-    momentsList.classList.toggle('hidden');
-    locationsList.classList.toggle('hidden');
-    
-    map.panTo([
-        locationRef.value.coordinates.lat,
-        locationRef.value.coordinates.lng
-    ])
-}
-
-function backToList() {
-    locationRef.value = {};
-    locationsList.classList.remove('hidden');
-    momentsList.classList.add('hidden');
-}
 
 </script>
 
 <template>
-    <Head title="Welcome" />
-
-    <main class="grid grid-cols-3 max-h-dvh h-dvh">
-        <div id="map" class="col-span-2"></div>
-    
-        <aside class="">
-            <ul class="p-4" id="locationsList">
-                <li v-for="pin in locations" :key="pin.id">
-                    <div @click="panTo(pin.id)" class="cursor-pointer text-2xl py-2">{{ pin.title }}</div>
-                </li>
-            </ul>
-            <div class="overflow-auto absolute hidden" id="momentsList">
-                <div @click="backToList" class=" cursor-pointer text-xl">Back to list ...</div>
-                <div 
-                    v-for="moment in locationRef.moments" 
-                    :key="moment.id"
-                    class="">
-                    <img :src="moment.image" alt="" loading="lazy" class=" h-48 w-full object-cover">
-                    <div class="m-4">
-                        <h3 class="truncate text-xl">{{ moment.title }}</h3>
-                        <div class="">Date Taken: {{ moment.date_taken }}</div>
-                        <!-- <div class="">Direction: {{ moment.direction }}</div>
-                        <div class="">Source: {{ moment.source }}</div>
-                        <div class="">Shared By: {{ moment.author.name }}</div>
-                        {{ moment.description }} -->
-                    </div>
-                </div>
-            </div>
-        </aside>
-    </main>
-
+    <div id="container">
+        <div id="mapContainer"></div>
+    </div>
 </template>
+
+<style scoped>
+    #mapContainer {
+        width: 80vw;
+        height: 100vh;
+    }
+</style>
