07c1840bfbc9c651c082dc55b10ef22c0b8a008a
jlabs <adams.j.6385@gmail.com>
2024-01-21 21:55:37 +0000

    Moment image included in card

--- resources/js/Pages/Locations/Edit.vue
+++ resources/js/Pages/Locations/Edit.vue
@@ -39,6 +39,7 @@ const form = useForm({
                             
                             <button type="submit" class="block bg-orange-500 p-2 rounded my-2" :disabled="form.processing">Save</button>
                         </form>
+                        <a :href="`/locations/${location.id}/moments/new`" class="block bg-green-300 p-2 rounded my-2 text-center text-black">Add moment</a>
                         <div class="flext flex-row gap-4">
                             <a 
                                 v-for="moment in location.moments" 
@@ -46,7 +47,12 @@ const form = useForm({
                                 :href="`/locations/${location.id}/moments/${moment.id}`" 
                                 class="block"
                             >
-                                {{ moment.id }} - {{  moment.title }}
+                                <div class="grid grid-flow-col grid-cols-2">
+                                    <img :src="moment.image" alt="">
+                                    <div class="">
+                                        {{ moment.title }}
+                                    </div>
+                                </div>
                             </a>
                         </div>
                     </div>
