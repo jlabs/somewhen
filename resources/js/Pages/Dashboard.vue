c7c99f474abddfc8fe25dfb69919d4795cce3862
jlabs <adams.j.6385@gmail.com>
2024-01-22 00:37:01 +0000

    Count locations on dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,13 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    locations: {
+        type: Number,
+        required: true,
+    },
+});
 </script>
 
 <template>
@@ -15,6 +22,7 @@ import { Head } from '@inertiajs/vue3';
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100">{{ locations }} Locations</div>
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,9 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return Inertia::render('Dashboard', [
+        'locationsCount' => Location::all()->count()
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
