e8c3119a652bdddfc66eedccd965e69e0e739b71
jlabs <adams.j.6385@gmail.com>
2024-01-28 22:06:43 +0000

    Link to locations page

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -21,7 +21,7 @@ const props = defineProps({
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">{{ locations }} Locations</div>
+                    <div class="p-6 text-gray-900 dark:text-gray-100"><a href="/locations">{{ locations }} Locations</a></div>
                 </div>
             </div>
         </div>
